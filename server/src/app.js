7908c4e8a11cddff21d256260dfa9fae9118204f
cmcarreiro <catarinaccarreiro@gmail.com>
2022-05-27 08:53:42 +0100

    Added morgan logging middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router')
 const app = express();
 
 app.use(cors());
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 

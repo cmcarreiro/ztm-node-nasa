4479a815c97f38115d131a6728bd4e1d88dae971
cmcarreiro <catarinaccarreiro@gmail.com>
2022-06-07 22:45:50 +0100

    [Launches] Created abort launch endpoint

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const { 
     getAllLaunches,
     addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById
  } = require('../../models/launches.model')
 
 
@@ -31,7 +33,23 @@ function httpAddNewLaunch(req,res) {
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    if (!existsLaunchWithId(launchId)) {
+        //if launch doesn't exist
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
+
+    const aborted = abortLaunchById(launchId);
+    //if launch does exist
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch
 };
